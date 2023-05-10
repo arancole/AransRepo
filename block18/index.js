af0e6087d13b78f240abbdbdf5a9fb2640fa6ffb
Aran Coleman <aranco121@gmail.com>
2023-05-09 21:29:16 -0400

    feat: homework for block18

---
+++ block18/index.js
@@ -0,0 +1,7 @@
+// Unit test specifications should take the form of:
+
+//     Expect [action] to be [some result]
+//     Example: For the prompt "A function called "addition" that returns the sum of two input integers", your tests might include:
+//         Expect addition(2, 3) to be a number
+//         Expect addition(2, 3) to be equal to 5
+//         Expect addition("a", 3) to be an error
