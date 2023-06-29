fc64fc44018c5204a32e32232f6912d78955d5ee
Aran Coleman <aranco121@gmail.com>
2023-06-29 19:19:27 -0400

    feat: block25gp

---
+++ block25/gp15/guidedpractice15/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <Button count={count} setCount={setCount} />
+    </div>
+  );
+}
+
+function Button(props) {
+  console.log("Props: ", props);
+  return (
+    <button
+      onClick={() => {
+        props.setCount(props.count);
+      }}
+    >
+      {props.count}
+    </button>
+  );
+}
+
+export default App;
